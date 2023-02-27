6068da2cf39b9f3b855e05f319fc06eb61394a08
Adriele Lima <adriele.p.t@hotmail.com>
2023-02-27 13:40:18 +0000

    Adding Favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -117,8 +117,8 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
     
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1; 
     document.getElementById('operator').textContent = "-";
 
 }
@@ -133,8 +133,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 function displayDivisionQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1; 
     document.getElementById('operator').textContent = "/";
 
 }
